2071ca0f30a0527ebbda1aa7a74421d4ec069147
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-12 12:43:35 +0530

    Add database connection setup in create_table.js

---
+++ create_table.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "taiyo3690",
+    port: '3306',
+    database: "college"
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("...database is connected!");
+    }
+});
