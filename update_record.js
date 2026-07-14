bf370b1cd484e55c134c8f3ac63c30978a0050c2
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-14 21:44:01 +0530

    Add SQL update logic to update_record.js

--- update_record.js
+++ update_record.js
@@ -13,6 +13,16 @@ con.connect(function(err) {
     } else {
         console.log("...database is connected!");
 
+        var sql = "UPDATE student SET scity = 'pune' WHERE sname = 'aaa'";
+        con.query(sql, function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("...Update specific record from the table!");
+                console.log(result);
+            }
+        });
+
         con.end(function(err) {
             if (err) {
                 console.log(err);
