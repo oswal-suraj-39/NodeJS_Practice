36bb4941343c07b0fb0a47ed22a8e3bbd62fc1d7
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-14 21:45:34 +0530

    Add initial delete_record.js file with MySQL connection setup

---
+++ delete_record.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "suraj3690",
+    port: "3306",
+    database: "college"
+});
