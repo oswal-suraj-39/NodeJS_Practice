0ab30111504bb8791e46c6b4f942183c884fd9ae
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-14 21:39:27 +0530

    Add connection and disconnection logic in insert_record.js

--- insert_record.js
+++ insert_record.js
@@ -7,3 +7,19 @@ var con = mysql.createConnection({
     port: '3306',
     database: "college"
 });
+
+con.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("...database is connected!");
+
+        con.end(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("...database is disconnected!");
+            }
+        });
+    }
+});
