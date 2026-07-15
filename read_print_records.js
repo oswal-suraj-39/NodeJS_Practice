093dcf52b1eed534281049a26861f45ed21f1695
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-15 23:03:36 +0530

    Add query execution to select and display all rows from the student table in read_print_records.js

--- read_print_records.js
+++ read_print_records.js
@@ -13,6 +13,21 @@ con.connect(function(err) {
     } else {
         console.log("...database is connected!");
 
+        var sql = "SELECT * From student";
+        con.query(sql, function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("...selected all rows from the table!");
+                console.log(" roll no | sName | sCity ");
+                console.log("-------------------------");
+                Object.keys(result).forEach(function (key) {
+                    var record = result[key];
+                    console.log("    "+record.rollno+"    | "+record.sname+"   |   "+record.scity);
+                });
+            }
+        });
+
         con.end(function(err) {
             if (err) {
                 console.log(err);
