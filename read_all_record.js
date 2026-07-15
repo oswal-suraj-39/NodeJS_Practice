a66b01c667919d257dd35403d1bfe21916733306
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-15 22:53:27 +0530

    Add initial setup for MySQL connection in read_all_record.js

---
+++ read_all_record.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    hort: "localhost",
+    user: "root",
+    password: "taiyo3690",
+    port: "3306",
+    database: "college"
+});
