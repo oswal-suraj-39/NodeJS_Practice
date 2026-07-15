44b356abe7b0ea64d27d7a0c4ab2124b5948525f
oswal-suraj-39 <surajoswal39@gmail.com>
2026-07-15 23:03:07 +0530

    Add connection logic and disconnection handling in read_print_records.js

--- read_print_records.js
+++ read_print_records.js
@@ -6,3 +6,19 @@ var con = mysql.createConnection({
     port: "3306",
     database: "college"
 });
+
+con.connect(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("...database is connected!");
+
+        con.end(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("...database is disconnected!");
+            }
+        });
+    }
+});
